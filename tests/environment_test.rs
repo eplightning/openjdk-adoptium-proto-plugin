974d19bfc8a5cd58ec7b263028e3e57ff7e56d10
Bartosz Sławianowski <wrexdot@gmail.com>
2026-04-25 20:57:21 +0200

    feat: JAVA_HOME support

---
+++ tests/environment_test.rs
@@ -0,0 +1,15 @@
+use proto_pdk_test_utils::*;
+
+mod openjdk_adoptium_tool {
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn sets_java_home() {
+        let sandbox = create_empty_proto_sandbox();
+        let plugin = sandbox.create_plugin("java-test").await;
+
+        let output = plugin.activate_environment(ActivateEnvironmentInput::default()).await;
+
+        assert!(output.env.contains_key("JAVA_HOME"));
+    }
+}
